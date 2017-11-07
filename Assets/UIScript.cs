aaee842dc3ecf65f980ddd96ff183a95fff1ae03
lessuknow <chenc18@rpi.edu>
2017-11-07 04:09:35 -0500

    Updated the UI.

--- Assets/UIScript.cs
+++ Assets/UIScript.cs
@@ -4,16 +4,29 @@ using UnityEngine;
 
 public class UIScript : MonoBehaviour {
 
-	public GameObject textBox, lifeSpan;
+	public GameObject textBox, lifeSpan, lives,key;
 	int border = 20;
+	int curKeyNum = 3;
 
 	// Use this for initialization
 	void Start () {
 		textBox.transform.position = new Vector3 (Screen.width/2 - border, Screen.height - textBox.GetComponent<RectTransform>().rect.height/2 - border, 0);
+
 		lifeSpan.transform.position = new Vector3 (Screen.width - lifeSpan.GetComponent<RectTransform>().rect.width/2 - border, 
 		Screen.height - lifeSpan.GetComponent<RectTransform>().rect.height/2 - border, 0);
 
+		lives.transform.position = new Vector3(lives.GetComponent<RectTransform>().rect.width - border,
+			lives.GetComponent<RectTransform>().rect.height - border, 0);
+		
+		for (int i = 0; i < curKeyNum; i++) {
+
+			GameObject tempKey = Instantiate (key,key.transform.parent);
 	
 
+			tempKey .transform.position = new Vector3 (key.GetComponent<RectTransform> ().rect.width - border/2 + key.GetComponent<RectTransform> ().rect.width * i,
+				lives.GetComponent<RectTransform> ().rect.height / 2 +
+				lives.transform.position.y / 2 +
+				key.GetComponent<RectTransform> ().rect.height, 0);
+		}
 	}
 }
