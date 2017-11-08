54c013eaf6b83245c21609c829f4a7ef16dfef48
lessuknow <chenc18@rpi.edu>
2017-11-08 12:07:27 -0500

    textCode

---
+++ Assets/TextInteract.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextInteract : InteractControl {
+
+	[SerializeField] TextboxScript txtscript;
+	[SerializeField] string textToPrint;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+	public override void doInteraction(Transform player) {
+		print ("Running itneract!");
+		if (player.gameObject.tag == "Player") {
+			txtscript.setText (textToPrint);
+		}
+	
+	}
+}
