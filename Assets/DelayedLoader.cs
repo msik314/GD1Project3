f6681312f704545aed5bbcb1bbad002e15d83614
msik314 <msik314@gmail.com>
2017-11-07 23:25:37 -0500

    Add intro scene

---
+++ Assets/DelayedLoader.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DelayedLoader : MonoBehaviour {
+    [SerializeField] private float loadDelay;
+    // Use this for initialization
+    int next;
+    
+    float time = 0;
+    void Awake()
+    {
+        next = SceneManager.GetActiveScene().buildIndex  + 1;
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if(time >= loadDelay)
+        {
+            SceneManager.LoadScene(next);
+        }
+        time += Time.deltaTime;
+    }
+}
